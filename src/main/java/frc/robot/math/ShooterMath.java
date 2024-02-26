417076b9b85c5b5704a37035e1436d97272a87c5
Spagrrt <80074923+Spagrrt@users.noreply.github.com>
2024-02-26 17:01:54 -0600

    Checked for valid orientations before auto command inits

--- src/main/java/frc/robot/math/ShooterMath.java
+++ src/main/java/frc/robot/math/ShooterMath.java
@@ -47,12 +47,14 @@ public class ShooterMath {
     }
 
     //TODO - Relies on getOrientation, which might not be initialized
+    //Safe for now
     public static double getXDiff(Pose2d robotPose) {
 //        return Constants.cSpeakerTargetPos.getX() - robotPose.getX();
         return getOrientation().getSpeakerTargetPos().getX() - robotPose.getX();
     }
 
     //TODO - Relies on getOrientation, which might not be initialized
+    //Safe for now
     public static double getYDiff(Pose2d robotPose) {
 //        return Constants.cSpeakerTargetPos.getY() - robotPose.getY();
         return getOrientation().getSpeakerTargetPos().getY() - robotPose.getY();
