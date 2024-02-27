018b0777d4f6dde98a85903b6fa9d60ae89cfb9b
Spagrrt <80074923+Spagrrt@users.noreply.github.com>
2024-02-27 17:18:05 -0600

    Made intake more reliable hopefully

--- src/main/java/frc/robot/commands/NoteLoadCommand.java
+++ src/main/java/frc/robot/commands/NoteLoadCommand.java
@@ -5,7 +5,9 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 public class NoteLoadCommand extends Command {
 
-    ShooterSubsystem shooterSubsystem;
+    private ShooterSubsystem shooterSubsystem;
+    private boolean isEscaped = true;
+
 
     public NoteLoadCommand(ShooterSubsystem shooterSubsystem) {
         this.shooterSubsystem = shooterSubsystem;
@@ -19,8 +21,18 @@ public class NoteLoadCommand extends Command {
     }
 
     @Override
+    public void execute() {
+        super.execute();
+        if (shooterSubsystem.getBeamBreak() && !isEscaped){
+            isEscaped = true;
+            shooterSubsystem.setShooterSpeed(0);
+            shooterSubsystem.setShooterControlSpeed(0.3);
+        }
+    }
+
+    @Override
     public boolean isFinished() {
-        return shooterSubsystem.getBeamBreak();
+        return isEscaped && !shooterSubsystem.getBeamBreak();
     }
 
     @Override
